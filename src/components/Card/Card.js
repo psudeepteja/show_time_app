5d4c6dde89f95ff5c0d1edcb7400b2f17233d55f
teja@171 <pandipatitejs@gmail.com>
2024-07-21 12:37:54 +0530

    fixes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -59,7 +59,6 @@ export default function Card({ nowShowingData }) {
               className="sm:w-72 lg:w-72 sm:h-72 md:h-96 object-cover"
               width={300}
               height={0}
-              // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
             <div className="p-4">
               <h6 className="text-sm lg:text-base font-semibold mb-2">{item.label}</h6>
